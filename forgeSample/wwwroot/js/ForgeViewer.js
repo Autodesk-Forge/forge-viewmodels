705fa8c5645e61b172b2ff1a80d6687256012b5d
Augusto Goncalves <augusto.goncalves@autodesk.com>
2019-03-26 10:21:08 -0300

    custom property panel + document browser

--- forgeSample/wwwroot/js/ForgeViewer.js
+++ forgeSample/wwwroot/js/ForgeViewer.js
@@ -36,7 +36,7 @@ function launchViewer(urn) {
   var documentId = 'urn:' + urn;
   Autodesk.Viewing.Initializer(options, function onInitialized() {
     viewerApp = new Autodesk.Viewing.ViewingApplication('forgeViewer');
-    viewerApp.registerViewer(viewerApp.k3D, Autodesk.Viewing.Private.GuiViewer3D);
+    viewerApp.registerViewer(viewerApp.k3D, Autodesk.Viewing.Private.GuiViewer3D, { extensions: ['CustomPropertyPanelExtension', 'Autodesk.DocumentBrowser'] });
     viewerApp.loadDocument(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
   });
 }
