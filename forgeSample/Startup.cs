ddf80a111bfd9b7a770311b2b98081f3e3081488
João Paulo de Ornelas Martins <joao.martins@unknown.home>
2021-02-04 11:53:56 -0300

    fix signalr

--- forgeSample/Startup.cs
+++ forgeSample/Startup.cs
@@ -12,7 +12,9 @@ namespace forgeSample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddNewtonsoftJson();
-            services.AddSignalR();
+            services.AddSignalR().AddNewtonsoftJsonProtocol(opt=> {
+                opt.PayloadSerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,14 +30,14 @@ namespace forgeSample
             }
 
             app.UseRouting();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+
             app.UseEndpoints(routes =>
             {
                 routes.MapHub<Controllers.ModelDerivativeHub>("/api/signalr/modelderivative");
             });
-
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
